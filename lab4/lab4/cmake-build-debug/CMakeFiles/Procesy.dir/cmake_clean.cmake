cec89c2ee0f79006a2f617fe9aa89216dd93b6a9
Maryna05 <luka4ik.marina@gmail.com>
2020-04-15 23:08:56 +0200

    add new files

---
+++ lab4/lab4/cmake-build-debug/CMakeFiles/Procesy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Procesy.dir/shell.c.o"
+  "CMakeFiles/Procesy.dir/funcs.c.o"
+  "CMakeFiles/Procesy.dir/save.c.o"
+  "Procesy.pdb"
+  "Procesy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Procesy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
